3c66d9cb33b7c5161aadd78cc64f4fa9f0918250
pozniej-znajde-wolne-haslo <aniarafeld@o2.pl>
2024-01-02 15:21:12 +0100

    adds authorization and role to the books-routes

--- routes/booksRoutes.js
+++ routes/booksRoutes.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { createBook } from '../controllers/booksControllers.js';
+import { authorization } from '../middleware/authorization.js';
+import { role } from '../middleware/role.js';
 
 const routes = Router();
 
-routes.post('/book', createBook);
+routes.post('/book', authorization, role, createBook);
 
 export default routes;
