3f6f3aba6d74cb1bdf956ef889895596c4f09fed
pozniej-znajde-wolne-haslo <aniarafeld@o2.pl>
2024-01-02 13:54:55 +0100

    creates server and mongoose connection

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import fileUpload from 'express-fileupload';
+import morgan from 'morgan';
+import { database } from './database/database.js';
+
+const app = express();
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+app.use(cors({ origin: `${process.env.HOST}`, exposedHeaders: ['token'] }));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('./views/public'));
+app.use(fileUpload());
+app.use(morgan('tiny'));
+
+database();
+
+app.listen(PORT, () => console.log('the server is running on port', PORT));
