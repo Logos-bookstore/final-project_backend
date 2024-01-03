6e0d499b45edbd6b902533da0bf0e5d6bc046733
anonymous <email>
2024-01-03 11:21:39 +0100

    create: book by genre route

--- routes/booksRoutes.js
+++ routes/booksRoutes.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { createBook } from '../controllers/booksControllers.js';
+import { createBook, genreBook } from '../controllers/booksControllers.js';
 
 const routes = Router();
 
 routes.post('/book', createBook);
+routes.get('/genre', genreBook);
 
 export default routes;
