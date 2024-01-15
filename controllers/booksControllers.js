684cadf959123b50e240d0eab22fa35356bfe17c
pozniej-znajde-wolne-haslo <aniarafeld@o2.pl>
2024-01-15 11:05:18 +0100

    expands delete-book-controller (deletes reviews to the book and references to them in user documents)

--- controllers/booksControllers.js
+++ controllers/booksControllers.js
@@ -1,5 +1,7 @@
 import BookModel from '../models/Book.js';
 import GenreModel from '../models/Genre.js';
+import ReviewModel from '../models/Review.js';
+import UserModel from '../models/User.js';
 
 const createBook = async (req, res, next) => {
   try {
@@ -95,6 +97,27 @@ const updateBook = async (req, res, next) => {
 
 const deleteBook = async (req, res, next) => {
   try {
+    // delete book's reviews and ref's in users' docs:
+    const findReviews = await ReviewModel.find({ book: req.params.id });
+    const del = (findReviews) => {
+      const promises = findReviews.map(async (i) => {
+        return await UserModel.updateOne(
+          { reviews: i._id },
+          { $pull: { reviews: i._id } },
+          { new: true }
+        );
+      });
+      return Promise.all(promises);
+    };
+    del(findReviews);
+    const delRev = (findReviews) => {
+      const promises = findReviews.map(async (i) => {
+        return await ReviewModel.findByIdAndDelete(i._id);
+      });
+      return Promise.all(promises);
+    };
+    delRev(findReviews);
+    // delete the book itself:
     await BookModel.findByIdAndDelete(req.params.id);
     res.json({ success: true, message: 'The book was successfully deleted.' });
   } catch (error) {
