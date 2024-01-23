eeda2317df0705330fd086258c39008c3bba2c06
anonymous <email>
2024-01-23 15:03:43 +0100

    update: someBook select author spelling mistake

--- controllers/genresControllers.js
+++ controllers/genresControllers.js
@@ -14,7 +14,7 @@ const someBook = async (req, res, next) => {
   try {
     const count = await BookModel.find().estimatedDocumentCount();
     const random = Math.floor(Math.random() * count) + 1;
-    const book = await BookModel.findOne().skip(random).select({title: 1, auhtor: 1, description: 1, avgRating: 1, price: 1, "image.thumbnail": 1});
+    const book = await BookModel.findOne().skip(random).select({title: 1, author: 1, description: 1, avgRating: 1, price: 1, "image.thumbnail": 1});
     res.json({success: true, data: book});
   } catch (error) {
     next(error);
